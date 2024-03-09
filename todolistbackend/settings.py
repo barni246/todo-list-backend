d391a9748c4503dcf860a9bb12844b9ac188c41d
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-03-09 17:31:02 +0100

    server connected

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,10 +25,12 @@ SECRET_KEY = 'django-insecure-kw=9%(kctv8u-$1owndrw=70&3_ocm)=&^c#ne1o-=&%$2q*fi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
+CORS_ALLOWED_ORIGINS = [    'http://localhost:4200',]
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
